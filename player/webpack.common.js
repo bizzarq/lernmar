c9056887622ffee40edac0597c97f4daa0220479
Dirk Luedtke <daimonjiclimber-codes@yahoo.com>
2024-12-17 16:48:34 +0100

    combine 3 lernmar project into one, step 1: move the player into a sub-folder player

--- player/webpack.common.js
+++ player/webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.ts',
+  entry: './src/player/main.ts',
   module: {
     rules: [
       {
@@ -16,7 +16,7 @@ module.exports = {
   },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist/js'),
+    path: path.resolve(__dirname, 'dist/player/js'),
     clean: true,
   },
 };
