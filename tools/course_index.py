468a964afbb64b34515187de98a5e84c0cfffc76
Dirk Luedtke <daimonjiclimber-codes@yahoo.com>
2024-12-31 18:07:08 +0100

    make CourseIndex a class, and also add a read function

--- tools/course_index.py
+++ tools/course_index.py
@@ -4,7 +4,7 @@ import sys
 import urllib
 import urllib.parse
 from xml.etree import ElementTree
-from typing import Any
+from typing import Any, Optional, TypeAlias
 
 
 def log(message: str) -> None:
@@ -14,65 +14,118 @@ def log(message: str) -> None:
     """
     print(message, file=sys.stderr)
 
-def generate_index(course_dir: str) -> list[dict[str, Any]]:
-    """
-    scans the courses in a directory and generates an index.
-    @param course_dir directory with courses.
-    @return index as a list of dictionaries.
-    """
-    courses = []
-    for name in os.listdir(course_dir):
-        path = os.path.join(course_dir, name)
-        if os.path.isdir(path):
-            course = scan_course(course_dir, path)
-            if course is not None:
-                courses.append(course)
-    return courses
 
-def write_index(course_dir: str, index: list[dict[str, Any]]) -> None:
-    """
-    generates an index.json file for an index.
-    @param course directory the index is for.
-    @param index index to write.
-    """
-    path = os.path.join(course_dir, 'index.json')
-    with open(path, mode='wt') as file:
-        json.dump({'courses': index}, file, indent=2)
+type Course = dict[str, Any]
 
-def scan_course(course_dir: str, course_path: str) -> dict[str, Any] | None:
+
+class CourseIndex:
     """
-    scans a course and generates and index entry for it.
-    @param: course_dir directory the course belongs to (which is used to generate relative paths).
-    @param: course_path path of course in file sytem.
-    @return index entry for course.
+    representation of a course index.
     """
-    manifest_path = os.path.join(course_path, 'imsmanifest.xml')
-    if not os.path.isfile(manifest_path):
-        return None
-    tree = ElementTree.parse(manifest_path)
-    root = tree.getroot()
-    elem = root.find('{http://www.imsglobal.org/xsd/imscp_v1p1}resources/{http://www.imsglobal.org/xsd/imscp_v1p1}resource')
-    if elem is None:
-        log(f'ignoring course {course_path} without resource element')
-        return None
-    href = elem.get('href')
-    if href is None:
-        log(f'ignoring course {course_path} without reference to index file')
-        return None
-    course_path = os.path.normpath(course_path)
-    parsed_href = urllib.parse.urlparse(href)
-    if parsed_href.scheme != '' or parsed_href.netloc != '':
-        log(f'ignoring course {course_path} without relative path reference')
-        return None
-    index_abs_path = os.path.normpath(os.path.join(course_path, parsed_href.path))
-    if not index_abs_path.startswith(course_path) or not os.path.isfile(index_abs_path):
-        log(f'ignoring course {course_path} whose index file is not within the course directory')
-        return None
-    name = os.path.basename(course_path)
-    index_url = urllib.parse.urlunparse((
-        '', '', f'{name}/{parsed_href.path}',
-        parsed_href.params, parsed_href.query, parsed_href.fragment))
-    return {'name': name, 'path': index_url}
+
+    def __init__(self, course_dir: Optional[str]=None) -> None:
+        """
+        constructor.
+        @param course_dir directory the index is for. if omitted, current directory.
+        """
+        if course_dir is None:
+            course_dir = os.getcwd()
+        self._course_dir = course_dir
+        self._courses = []
+
+    def add_course(self, course: Course) -> None:
+        """
+        adds a course to the index.
+        if there is already a course with the same name, the old entry will be deleted.
+        @param course course to add.
+        """
+        for old_course_id, old_course in enumerate(self._courses):
+            if course['name'] == old_course['name']:
+                del self._courses[old_course_id]
+                break
+        self._courses.append(course)
+
+    def read(self, path: Optional[str]=None) -> None:
+        """
+        reads an index from a file.
+        @param path path of index file. "index.json" in course directory if omitted.
+        """
+        if path is None:
+            path = os.path.join(self._course_dir, 'index.json')
+        with open(path, mode='rt') as file:
+            data = json.load(file)
+            for course in data['courses']:
+                if isinstance(course.get('name'), str) and isinstance(course.get('path'), str):
+                    self.add_course(course)
+                else:
+                    log(f'ignoring invalid course entry {course}')
+
+    def write(self, path: Optional[str]=None) -> None:
+        """
+        writes the index to a file.
+        @param path path of index file. "index.json" in course directory if omitted.
+        """
+        if path is None:
+            path = os.path.join(self._course_dir, 'index.json')
+        with open(path, mode='wt') as file:
+            json.dump({'courses': self._courses}, file, indent=2)
+
+    def clear(self) -> None:
+        """
+        clears the index.
+        """
+        self._courses.clear()
+
+    def from_course_dir(self, course_dir: Optional[str]=None) -> None:
+        """
+        generates an index for the course directory. existing entries will be deleted.
+        scans the courses in a directory and generates an index.
+        @param course_dir directory with courses. if omitted, course directory of index.
+        @return course index.
+        """
+        if course_dir is None:
+            course_dir = self._course_dir
+        self.clear()
+        for name in os.listdir(course_dir):
+            path = os.path.join(course_dir, name)
+            if os.path.isdir(path):
+                course = self._scan_course(path)
+                self.add_course(course)
+        return index
+
+    def _scan_course(self, course_path: str) -> dict[str, Any] | None:
+        """
+        scans a course and generates and index entry for it.
+        @param: course_path path of course in file sytem.
+        @return index entry for course.
+        """
+        manifest_path = os.path.join(course_path, 'imsmanifest.xml')
+        if not os.path.isfile(manifest_path):
+            return None
+        tree = ElementTree.parse(manifest_path)
+        root = tree.getroot()
+        elem = root.find('{http://www.imsglobal.org/xsd/imscp_v1p1}resources/{http://www.imsglobal.org/xsd/imscp_v1p1}resource')
+        if elem is None:
+            log(f'ignoring course {course_path} without resource element')
+            return None
+        href = elem.get('href')
+        if href is None:
+            log(f'ignoring course {course_path} without reference to index file')
+            return None
+        course_path = os.path.normpath(course_path)
+        parsed_href = urllib.parse.urlparse(href)
+        if parsed_href.scheme != '' or parsed_href.netloc != '':
+            log(f'ignoring course {course_path} without relative path reference')
+            return None
+        index_abs_path = os.path.normpath(os.path.join(course_path, parsed_href.path))
+        if not index_abs_path.startswith(course_path) or not os.path.isfile(index_abs_path):
+            log(f'ignoring course {course_path} whose index file is not within the course directory')
+            return None
+        name = os.path.basename(course_path)
+        index_url = urllib.parse.urlunparse((
+            '', '', f'{name}/{parsed_href.path}',
+            parsed_href.params, parsed_href.query, parsed_href.fragment))
+        return {'name': name, 'path': index_url}
 
 
 if __name__ == '__main__':
@@ -87,5 +140,7 @@ if __name__ == '__main__':
     if course_dir is None:
         root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         course_dir = os.path.join(root_dir, 'dist', 'player', 'courses')
-    index = generate_index(course_dir)
-    write_index(course_dir, index)
+
+    index = CourseIndex(course_dir)
+    index.from_course_dir()
+    index.write()
