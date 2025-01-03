17df135c317876c2ce72ad7f1ba8e803cb4b850d
Dirk Luedtke <daimonjiclimber-codes@yahoo.com>
2025-01-03 14:54:18 +0100

    allow nested courses

--- src/wrapper/Course.ts
+++ src/wrapper/Course.ts
@@ -3,53 +3,87 @@ import type { ActivityState } from "./ActivityState";
 import type { ExecutableCourse } from "./CourseExecutor";
 
 
+type CoursePart = Activity | Course;
+
+function isActivity(part: CoursePart): part is Activity {
+  return "isMandatory" in part;
+}
+
+
+/**
+ * standard implementation of a lernmar course. A course is a collection of activities which can
+ * be executed by a course executer. courses can contain other courses. In this case, each of the
+ * parts (activities and sub-courses) need a unique name.
+ */
 class Course implements ExecutableCourse {
+  readonly name: string;
   #section;
-  #nameActivities: Record<string, Activity>;
-  #incompletes: Array<string>;
+  #nameParts: Record<string, CoursePart>;
+  #incompletes: Array<CoursePart>;
   #incompleteId: number;
   #mandatoryActivities;
+  #namePattern = /^([^\.]+)\.?(.*)$/
 
   /**
-   * constructor
+   * constructor.
    * @param section HTML element the activities can insert their content.
-   * @param activities activities, the course consist of. the names of the activities must be
+   * @param parts activities, the course consist of. the names of the activities must be
    *   unique. the name "intro" is reserved for activities which are called in the very beginning
    *   (before the connection to the learn management system is completely established).
+   * @param name name of the course. Must be unique when course is part of a larger course.
+   *   default name is "Main Course".
    */
-  constructor(section: HTMLElement, activities: Array<Activity>) {
+  constructor(section: HTMLElement, parts: Array<CoursePart>, name?: string) {
+    this.name = name === undefined ? "Main Course": name;
     this.#section = section;
-    this.#nameActivities = {};
+    this.#nameParts = {};
     this.#incompletes = [];
     this.#incompleteId = 0;
     this.#mandatoryActivities = 0;
-    for (let activity of activities) {
-      if (activity.name in this.#nameActivities) {
-        console.error(`ignore duplicate activity ${activity.name}`);
+    for (let part of parts) {
+      if (part.name in this.#nameParts) {
+        console.error(`ignore duplicate activity ${part.name}`);
         continue;
       }
-      this.#nameActivities[activity.name] = activity;
-      this.#incompletes.push(activity.name);
-      if (activity.isMandatory) {
-        this.#mandatoryActivities++;
+      this.#nameParts[part.name] = part;
+      this.#incompletes.push(part);
+      if (isActivity(part)) {
+        if (part.isMandatory) {
+          this.#mandatoryActivities++;
+        }
+      }
+      else {
+        this.#mandatoryActivities += part.mandatoryActivities();
       }
     }
   }
 
   async executeActivity(name: string): Promise<ActivityState> {
-    let activity = this.#nameActivities[name];
-    if (activity) {
-      let state = await activity.execute(this.#section);
-      if (state.complete) {
-        // if activity was complete, remove it from activity list.
-        // the incompleteId will then point to the next incomplete activity (or behind the end).
-        let incompleteId = this.#incompletes.indexOf(name);
-        if (incompleteId >= 0) {
-          this.#incompletes.splice(incompleteId, 1);
+    let match = name.match(this.#namePattern);
+    if (match) {
+      let nameHead = match[1];
+      let nameTail = match[2]; // should be empty for activities
+      let part = this.#nameParts[nameHead];
+      if (part) {
+        if (isActivity(part)) {
+          let state = await part.execute(this.#section);
+          if (state.complete) {
+            // if activity was complete, remove it from activity list.
+            // the incompleteId will then point to the next incomplete activity (or behind the end).
+            let incompleteId = this.#incompletes.indexOf(part);
+            if (incompleteId >= 0) {
+              this.#incompletes.splice(incompleteId, 1);
+            }
+          }
+          return state;
+        }
+        else {
+          // whether sub-course is complete will be visible at next nextActivity call.
+          return await part.executeActivity(nameTail)
         }
       }
-      return state;
     }
+    // if name could not be matched
     return { mandatory: false, complete: true, success: false };
   }
 
@@ -60,13 +94,30 @@ class Course implements ExecutableCourse {
   async finalize(): Promise<void> {}
 
   nextActivity(): string {
-    if (this.#incompletes.length > 0) {
+    while (this.#incompletes.length > 0) {
       if (this.#incompleteId >= this.#incompletes.length) {
         // if the last incomplete was deleted, the id points behind the last id.
         // in this case, we re-start with the first incomplete.
         this.#incompleteId = 0;
       }
-      return this.#incompletes[this.#incompleteId];
+      let part = this.#incompletes[this.#incompleteId];
+      if (isActivity(part)) {
+        return part.name;
+      }
+      else {
+        let name = part.nextActivity();
+        if (name === "") {
+          // if sub-course is complete call finalize and deleted it from incomplete list
+          part?.finalize();
+          let incompleteId = this.#incompletes.indexOf(part);
+          if (incompleteId >= 0) {
+            this.#incompletes.splice(incompleteId, 1);
+          }
+          // as sub-course was complete we need to find another next Activity (continue loop)
+          continue;
+        }
+        return `${part.name}.${name}`;
+      }
     }
     // no incompletes left, return empty string for informing that course is complete.
     return "";
--- test/wrapper/Course.test.ts
+++ test/wrapper/Course.test.ts
@@ -82,4 +82,56 @@ test('incomplete activities', async () => {
   
   expect(course.nextActivity()).toBe("");
 });
-  
+
+test('empty course', async () => {
+  let section = document.createElement("section");
+  let course = new Course(section, []);
+  expect(course.mandatoryActivities()).toBe(0);
+  expect(course.nextActivity()).toBe("");
+  let result = await course.executeActivity("unknown activity");
+  expect(result).toMatchObject({ mandatory: false, complete: true, success: false });
+  expect(course.nextActivity()).toBe("");
+});
+
+test('nested course, normal execution', async () => {
+  let section = document.createElement("section");
+  let activity1 = new TestActivity("activity 1", true);
+  let activity211 = new TestActivity("activity 211", true);
+  let course21 = new Course(section, [activity211], "course 21");
+  let activity22 = new TestActivity("activity 22", true);
+  let course2 = new Course(section, [course21, activity22], "course 2");
+  let course = new Course(section, [activity1, course2]);
+
+  expect(course21.mandatoryActivities()).toBe(1);
+  expect(course2.mandatoryActivities()).toBe(2);
+  expect(course.mandatoryActivities()).toBe(3);
+
+  expect(course21.nextActivity()).toBe("activity 211");
+  expect(course2.nextActivity()).toBe("course 21.activity 211");
+  expect(course.nextActivity()).toBe("activity 1");
+  let result = await course.executeActivity("unknown activity");
+  expect(result).toMatchObject({ mandatory: false, complete: true, success: false });
+
+  expect(course21.nextActivity()).toBe("activity 211");
+  expect(course2.nextActivity()).toBe("course 21.activity 211");
+  expect(course.nextActivity()).toBe("activity 1");
+  result = await course.executeActivity("activity 1");
+  expect(result).toMatchObject({ mandatory: true, complete: true, success: true });
+
+  expect(course21.nextActivity()).toBe("activity 211");
+  expect(course2.nextActivity()).toBe("course 21.activity 211");
+  expect(course.nextActivity()).toBe("course 2.course 21.activity 211");
+  result = await course.executeActivity("course 2.course 21.activity 211");
+  expect(result).toMatchObject({ mandatory: true, complete: true, success: true });
+
+  expect(course21.nextActivity()).toBe("");
+  expect(course2.nextActivity()).toBe("activity 22");
+  expect(course.nextActivity()).toBe("course 2.activity 22");
+  result = await course.executeActivity("course 2.activity 22");
+  expect(result).toMatchObject({ mandatory: true, complete: true, success: true });
+
+  expect(course21.nextActivity()).toBe("");
+  expect(course2.nextActivity()).toBe("");
+  expect(course.nextActivity()).toBe("");
+});
+
