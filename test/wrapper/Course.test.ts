156e3b6a174af4ee719c8a8780b1b87dbc0a771b
Dirk Luedtke <daimonjiclimber-codes@yahoo.com>
2025-01-03 12:49:18 +0100

    test cases for changed execution order and handling of incomplete activities

--- test/wrapper/Course.test.ts
+++ test/wrapper/Course.test.ts
@@ -31,3 +31,55 @@ test('normal execution', async () => {
 
   expect(course.nextActivity()).toBe("");
 });
+
+test('changed execution order', async () => {
+  let section = document.createElement("section");
+  let activity1 = new TestActivity("activity 1", true);
+  let activity2 = new TestActivity("activity 2", true);
+  let activity3 = new TestActivity("activity 3", true);
+  let course = new Course(section, [activity1, activity2, activity3]);
+
+  expect(course.nextActivity()).toBe("activity 1");
+  let result = await course.executeActivity("activity 2");
+  expect(result).toMatchObject({ mandatory: true, complete: true, success: true });
+
+  expect(course.nextActivity()).toBe("activity 1");
+  result = await course.executeActivity("activity 3");
+  expect(result).toMatchObject({ mandatory: true, complete: true, success: true });
+
+  expect(course.nextActivity()).toBe("activity 1");
+  result = await course.executeActivity("activity 1");
+  expect(result).toMatchObject({ mandatory: true, complete: true, success: true });
+
+  expect(course.nextActivity()).toBe("");
+});
+
+test('incomplete activities', async () => {
+  let section = document.createElement("section");
+  let activity1 = new TestActivity("activity 1", true);
+  let activity2 = new TestActivity("activity 2", true);
+  let course = new Course(section, [activity1, activity2]);
+
+  activity1.result = { mandatory: true, complete: false };
+  expect(course.nextActivity()).toBe("activity 1");
+  let result = await course.executeActivity("activity 1");
+  expect(result).toMatchObject({ mandatory: true, complete: false });
+
+  activity1.result = { mandatory: true, complete: true, success: false };
+  expect(course.nextActivity()).toBe("activity 1");
+  result = await course.executeActivity("activity 1");
+  expect(result).toMatchObject({ mandatory: true, complete: true, success: false });
+  
+  activity2.result = { mandatory: true, complete: false };
+  expect(course.nextActivity()).toBe("activity 2");
+  result = await course.executeActivity("activity 2");
+  expect(result).toMatchObject({ mandatory: true, complete: false });
+
+  activity2.result = { mandatory: true, complete: true, success: true };
+  expect(course.nextActivity()).toBe("activity 2");
+  result = await course.executeActivity("activity 2");
+  expect(result).toMatchObject({ mandatory: true, complete: true, success: true });
+  
+  expect(course.nextActivity()).toBe("");
+});
+  
